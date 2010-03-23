c445569500fcf66a1301cfba0147500ae8b77463
Miles Gould <mgould1@sentinel.inf.ed.ac.uk>
2010-03-23 11:41:41 +0000

    Fixed amusing typo.

--- lib/Hook/Fork.pm
+++ lib/Hook/Fork.pm
@@ -117,7 +117,7 @@ Hook::Fork - automatically run code after a fork
 =head1 DESCRIPTION
 
 Forking can often confuse modules; if a parent opens a socket and sets
-come code to run at DESTROY to close it, that DESTROY will run in both
+some code to run at DESTROY to close it, that DESTROY will run in both
 the parent in the child.  This means the child exiting can mess up the
 parent, or the parent exiting can mess up the child.
 
